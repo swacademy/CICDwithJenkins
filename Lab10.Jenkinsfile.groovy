552361fda18da7cfb716706cc8c196020861b9c9
swacademy <blueexpert@hotmail.com>
2024-02-23 11:00:57 +0900

    Lab10.

---
+++ Lab10.Jenkinsfile.groovy
@@ -0,0 +1,55 @@
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url:'https://github.com/expertman/guestbook.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh './mvnw clean compile'
+            }
+        }
+        stage('Unit Test') {
+            steps {
+                sh './mvnw test'
+            }
+            
+            post {
+                always {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                }
+            }
+        }
+
+        stage('SonarQube Analysis') {
+            steps{
+                withSonarQubeEnv('SonarQube-Server'){
+                    sh '''
+                        ./mvnw sonar:sonar \
+                        -Dsonar.projectKey=guestbook \
+                        -Dsonar.host.url=http://43.201.49.180:9000 \
+                        -Dsonar.login=d7c0156ac596ee750c5583b68e6b051e87d61eb1
+                    '''
+                }
+            }
+        }
+        stage('SonarQube Quality Gate'){
+            steps{
+                timeout(time: 1, unit: 'MINUTES') {
+                    script{
+                        def qg = waitForQualityGate()
+                        if(qg.status != 'OK') {
+                            echo "NOT OK Status: ${qg.status}"
+                            error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                        } else{
+                            echo "OK Status: ${qg.status}"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
