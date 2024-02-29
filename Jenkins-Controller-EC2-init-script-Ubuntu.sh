4ef546fbadb58f87b8cd94eff67407646f40d24d
swacademy <blueexpert@hotmail.com>
2024-02-29 14:45:01 +0900

    Jenkins-Controller-EC2-init-script-Ubuntu.sh

---
+++ Jenkins-Controller-EC2-init-script-Ubuntu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt-get update -y
+
+# Java 17 Installation
+sudo wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
+	echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
+sudo apt-get update -y ; sudo apt-get install -y java-17-amazon-corretto-jdk
+
+# Docker Installation
+sudo apt-get install ca-certificates curl gnupg lsb-release apt-transport-https -y
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+sudo systemctl start docker
+sudo usermod -aG docker ubuntu
+sudo docker version
+
+sudo docker container run -d \
+    --name=mysqldb \
+    --restart=always \
+    -e MYSQL_ROOT_PASSWORD=education \
+    -e MYSQL_DATABASE=guestbook \
+    -p 3306:3306 \
+    yu3papa/mysql_hangul:2.0
